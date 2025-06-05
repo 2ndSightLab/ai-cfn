254a371e7b3e9c958514b1985c7aed62e5923ea5
Teri Radichel <github@pwn.fyi>
2025-06-05 19:01:20 -0400

    Update deploy.sh

--- staticweb/deploy.sh
+++ staticweb/deploy.sh
@@ -29,7 +29,23 @@ CLOUDFRONT_STACK="${STACK_NAME_PREFIX}-cloudfront"
 stack_exists() {
   local stack_name=$1
   if aws cloudformation describe-stacks --stack-name $stack_name &>/dev/null; then
-    return 0  # Stack exists
+  
+    # Check if stack exists in a failed state
+    echo "Checking for existing CloudFormation stack..."
+    STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 2>/dev/null || echo "STACK_NOT_FOUND")
+    
+    if [[ "$STACK_STATUS" == *"FAILED"* || "$STACK_STATUS" == *"ROLLBACK_COMPLETE"* ]]; then
+        echo "Stack $TLS_CERTIFICATE_STACK exists in a failed state ($STACK_STATUS)."
+        echo "Deleting it before redeployment..."
+        aws cloudformation delete-stack --stack-name $stack_name
+        
+        echo "Waiting for stack deletion to complete..."
+        aws cloudformation wait stack-delete-complete --stack-name $TLS_CERTIFICATE_STACK
+        echo "Stack deletion complete."
+     fi
+    
+     return 0  # Stack exists
+       
   else
     return 1  # Stack does not exist
   fi
