63e336bdde86103903e6a7b5a7f7f4c9bb0e7afc
Teri Radichel <github@pwn.fyi>
2025-06-05 23:28:28 -0400

    Update deploy.sh

--- staticweb/deploy.sh
+++ staticweb/deploy.sh
@@ -32,6 +32,7 @@ stack_exists() {
   if aws cloudformation describe-stacks --stack-name $stack_name &>/dev/null; then
   
     # Check if stack exists in a failed state
+    echo "Checking to see if stack exists in stack_exists function"
     STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 2>/dev/null || echo "STACK_NOT_FOUND")
     return 0  # Stack exists
        
