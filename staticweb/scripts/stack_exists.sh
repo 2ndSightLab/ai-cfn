f0ef97f787b93ad4fe9e55a22f795509ccc601c5
Teri Radichel <github@pwn.fyi>
2025-06-06 23:41:49 -0400

    Create stack_exists.sh

---
+++ staticweb/scripts/stack_exists.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "stack_exists.sh"
+
+stack_exists() {
+  local stack_name=$1
+  if aws cloudformation describe-stacks --stack-name $stack_name &>/dev/null; then
+  
+    # Check if stack exists in a failed state
+    echo "Checking to see if stack exists in stack_exists function"
+    STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 2>/dev/null || echo "STACK_NOT_FOUND")
+    return 0  # Stack exists
+       
+  else
+    return 1  # Stack does not exist
+  fi
+}
