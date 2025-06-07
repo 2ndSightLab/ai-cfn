7edbf18bb756810fecc8b32a3aa64470feeb2029
Teri Radichel <github@pwn.fyi>
2025-06-07 01:40:39 -0400

    Update deploy-tls-cert.sh

--- staticweb/scripts/deploy-tls-cert.sh
+++ staticweb/scripts/deploy-tls-cert.sh
@@ -65,7 +65,7 @@ if [[ "$DEPLOY_CERTIFICATE" == "y" || "$DEPLOY_CERTIFICATE" == "Y" ]]; then
     fi
     
     # Check if stack exists and delete if failed
-    delete-failed-stack-if-exists "$TLS_CERTIFICATE_STACK" $REGION
+    delete_failed_stack_if_exists $TLS_CERTIFICATE_STACK $REGION
   
     # Deploy the certificate using CloudFormation in the background
     # Use nohup to ensure the process continues even if the terminal is closed
