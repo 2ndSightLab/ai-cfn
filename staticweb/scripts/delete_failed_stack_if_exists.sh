91834328b48e6381cfceffe395a7df53e1bfb954
Teri Radichel <github@pwn.fyi>
2025-06-07 00:56:22 -0400

    Update delete_failed_stack_if_exists.sh

--- staticweb/scripts/delete_failed_stack_if_exists.sh
+++ staticweb/scripts/delete_failed_stack_if_exists.sh
@@ -2,20 +2,24 @@
 echo "delete_failed_stack_if_exists.sh"
 
 delete_failed_stack_if_exists() {
-  local stack_name=$1
-  if aws cloudformation describe-stacks --stack-name $stack_name &>/dev/null; then
+  local stack_name="$1"
+  local region="$2"
+
+  if [ "$region" == "" ]; then region="us-east-1"; fi
+  
+  if aws cloudformation describe-stacks --stack-name $stack_name --region $region &>/dev/null; then
   
     echo "Checking for existing CloudFormation stack..."
-    STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 2>/dev/null || echo "STACK_NOT_FOUND")
+    STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --region $region --output text 2>/dev/null || echo "STACK_NOT_FOUND")
     
     if [[ "$STACK_STATUS" == *"FAILED"* || "$STACK_STATUS" == *"ROLLBACK_COMPLETE"* ]]; then
         echo "Check if status exists in a failed state"
         echo "Stack $TLS_CERTIFICATE_STACK exists in a failed state ($STACK_STATUS)."
         echo "Deleting it before redeployment..."
-        aws cloudformation delete-stack --stack-name $stack_name
+        aws cloudformation delete-stack --stack-name $stack_name --region $region
         
         echo "Waiting for stack deletion to complete..."
-        aws cloudformation wait stack-delete-complete --stack-name $TLS_CERTIFICATE_STACK
+        aws cloudformation wait stack-delete-complete --stack-name $TLS_CERTIFICATE_STACK --region $region
         echo "Stack deletion complete."
      fi
   fi
