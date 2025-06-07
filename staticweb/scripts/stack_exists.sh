d04119fe00c98fd8f10053c40c32c57b03bf68b4
Teri Radichel <github@pwn.fyi>
2025-06-07 01:02:28 -0400

    Update stack_exists.sh

--- staticweb/scripts/stack_exists.sh
+++ staticweb/scripts/stack_exists.sh
@@ -2,12 +2,18 @@
 echo "stack_exists.sh"
 
 stack_exists() {
-  local stack_name=$1
-  if aws cloudformation describe-stacks --stack-name $stack_name &>/dev/null; then
+  local stack_name="$1"
+  local region="$2"
+
+  if [ "$region" == "" ]; then
+     echo "Region not set checking to see if $stack_name exists"; exit
+  fi
+  
+  if aws cloudformation describe-stacks --stack-name $stack_name --region $region &>/dev/null; then
   
     # Check if stack exists in a failed state
     echo "Checking to see if stack exists in stack_exists function"
-    STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 2>/dev/null || echo "STACK_NOT_FOUND")
+    STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --region $region --output text 2>/dev/null || echo "STACK_NOT_FOUND")
     return 0  # Stack exists
        
   else
