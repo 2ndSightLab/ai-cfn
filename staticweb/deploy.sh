3ca876900ff49bc7dedb80dc529787481b12617d
Teri Radichel <github@pwn.fyi>
2025-06-07 02:07:46 -0400

    Update deploy.sh

--- staticweb/deploy.sh
+++ staticweb/deploy.sh
@@ -33,11 +33,11 @@ source ./scripts/deploy-hosted-zone.sh
 source ./scripts/check-certificate-exists.sh
 source ./scripts/delete-existing-certificates.sh
 source ./scripts/deploy-tls-cert.sh
-source ./scripts/deploy-s3-content-bucket.sh
 source ./scripts/deploy-s3-access-log-bucket.sh
 source ./scripts/deploy-cloudfront-logs-bucket.sh
+source ./scripts/deploy-s3-content-bucket.sh
 source ./scripts/deploy-cloudfront-distribution.sh
-source ./scripts/deploy-validation-dns-records.sh
+
 
 echo "Deployment complete!"
 echo "Website URL: https://$DOMAIN_NAME"
