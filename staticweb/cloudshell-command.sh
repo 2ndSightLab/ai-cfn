32fb61fdb92bb8a36c851aec391607cfd69a0929
Teri Radichel <github@pwn.fyi>
2025-06-07 00:33:54 -0400

    Update cloudshell-command.sh

--- staticweb/cloudshell-command.sh
+++ staticweb/cloudshell-command.sh
@@ -1,7 +1,16 @@
+#use these commands to run the scripts in the AWS account where the website exists
 cd ~
 rm -rf ai-cfn
 git clone https://github.com/2ndSightLab/ai-cfn.git
 cd ai-cfn/staticweb
 chmod 700 deploy.sh
 chmod 700 scripts/deploy-tls-cert-validation.sh
+./deploy.sh
+
+#use these commands to run the script to update the name servers where the primary domain exists
+cd ~
+rm -rf ai-cfn
+git clone https://github.com/2ndSightLab/ai-cfn.git
+cd ai-cfn/staticweb
+chmod 700 update-nameservers.sh
 ./deploy.sh
