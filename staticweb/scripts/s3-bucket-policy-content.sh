c4e69faab9ddebe11a73a51674e7187dccef2ed2
Teri Radichel <github@pwn.fyi>
2025-06-09 18:59:32 -0400

    Update s3-bucket-policy-content.sh

--- staticweb/scripts/s3-bucket-policy-content.sh
+++ staticweb/scripts/s3-bucket-policy-content.sh
@@ -8,7 +8,7 @@ aws cloudformation deploy \
     --parameter-overrides \
       BucketName=$S3_BUCKET_NAME \
       OriginAccessIdentityId=$OAI_ID \
-      
+      CloudFrontDistributionID=$CLOUDFRONT_DISTRIBUTION_ID \
     --no-fail-on-empty-changeset
     
 stack_exists $S3_POLICY_WEBSITE_STACK $REGION
