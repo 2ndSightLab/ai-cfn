11cef0c7f65c2b7a6d6ca163275feab21e61ede9
Teri Radichel <github@pwn.fyi>
2025-06-09 03:58:52 -0400

    Update deploy-cloudfront-logs-bucket.sh

--- staticweb/scripts/deploy-cloudfront-logs-bucket.sh
+++ staticweb/scripts/deploy-cloudfront-logs-bucket.sh
@@ -5,17 +5,14 @@ echo "deploy-cloudfront-logs-bucket.sh"
 
 BUCKET_NAME=${BUCKET_NAME:-"${DOMAIN_NAME}-cloudfront-logs"}
 # CloudFront Logs Bucket
+
 read -p "Deploy CloudFront Logs Bucket? (y/n): " DEPLOY_CF_LOGS
 if [[ "$DEPLOY_CF_LOGS" == "y" || "$DEPLOY_CF_LOGS" == "Y" ]]; then
-  read -p "CloudFront logs retention days (default: 365): " CF_LOG_RETENTION_DAYS
-  CF_LOG_RETENTION_DAYS=${CF_LOG_RETENTION_DAYS:-365}
-  
-  read -p "Days before transitioning to Standard-IA (default: 30): " TRANSITION_STANDARD_IA_DAYS
-  TRANSITION_STANDARD_IA_DAYS=${TRANSITION_STANDARD_IA_DAYS:-30}
-  
-  read -p "Days before transitioning to Glacier (default: 90): " TRANSITION_GLACIER_DAYS
-  TRANSITION_GLACIER_DAYS=${TRANSITION_GLACIER_DAYS:-90}
 
+  # Not really used - nee to fix
+  read -p "CloudFront logs retention days (default: 90): " CF_LOG_RETENTION_DAYS
+  CF_LOG_RETENTION_DAYS=${CF_LOG_RETENTION_DAYS:-90}
+  
   delete_failed_stack_if_exists $CLOUDFRONT_LOGS_STACK $REGION
   echo "Deploying CloudFront Logs Bucket..."
   aws cloudformation deploy \
