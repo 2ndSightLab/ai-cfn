22932e850023d3b9f3b1d5da35729394f2288f70
Teri Radichel <github@pwn.fyi>
2025-06-09 17:47:22 -0400

    Update deploy-cloudfront-access.sh

--- staticweb/scripts/deploy-cloudfront-access.sh
+++ staticweb/scripts/deploy-cloudfront-access.sh
@@ -66,13 +66,13 @@ else
   echo "OAI ID: $OAI_ID"
 fi
 
-  delete_failed_stack_if_exists $S3_POLICY_WEBSITE_STACK
+delete_failed_stack_if_exists $S3_POLICY_WEBSITE_STACK
   
-  aws cloudformation deploy \
+aws cloudformation deploy \
     --template-file cfn/s3-bucket-policy-web.yaml \
     --stack-name $S3_POLICY_WEBSITE_STACK \
     --parameter-overrides \
       BucketName=$S3_BUCKET_NAME \
     --no-fail-on-empty-changeset
     
-  stack_exists $S3_POLICY_WEBSITE_STACK $REGION
+stack_exists $S3_POLICY_WEBSITE_STACK $REGION
