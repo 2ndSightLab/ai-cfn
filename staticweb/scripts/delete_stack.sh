dd59a82064c1fcca46bdc48216bad110c08ee254
Teri Radichel <github@pwn.fyi>
2025-06-09 16:20:15 -0400

    Update delete_stack.sh

--- staticweb/scripts/delete_stack.sh
+++ staticweb/scripts/delete_stack.sh
@@ -3,13 +3,16 @@
 delete_stack() {
   local stack_name=$1
   
-  # Delete the stack
-  echo "Deleting stack $stack_name..."
-  aws cloudformation delete-stack --stack-name $stack_name
+  # Check if the stack exists
+  if aws cloudformation describe-stacks --stack-name $stack_name &>/dev/null; then
+    # Delete the stack
+    echo "Deleting stack $stack_name..."
+    aws cloudformation delete-stack --stack-name $stack_name
 
-  # Wait for the stack deletion to complete
-  echo "Waiting for stack $stack_name deletion to complete..."
-  aws cloudformation wait stack-delete-complete --stack-name $stack_name
+    # Wait for the stack deletion to complete
+    echo "Waiting for stack $stack_name deletion to complete..."
+    aws cloudformation wait stack-delete-complete --stack-name $stack_name
 
-  echo "Stack $stack_name has been deleted successfully."
+    echo "Stack $stack_name has been deleted successfully."
+  fi
 }
