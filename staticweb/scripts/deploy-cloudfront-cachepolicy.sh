a10989b5ad38f2368449b1de1209822dffe785f3
Teri Radichel <github@pwn.fyi>
2025-06-10 16:23:55 -0400

    Update deploy-cloudfront-cachepolicy.sh

--- staticweb/scripts/deploy-cloudfront-cachepolicy.sh
+++ staticweb/scripts/deploy-cloudfront-cachepolicy.sh
@@ -9,7 +9,13 @@ if [[ "$DEPLOY_POLICY" == "y" || "$DEPLOY_POLICY" == "Y" ]]; then
     --template-file cfn/cloudfront-cachedisabledpolicy.yaml \
     --stack-name $CLOUDFRONT_CACHE_POLICY_STACK \
     --no-fail-on-empty-changeset
-  
+    
 fi
 
 stack_exists $CLOUDFRONT_CACHE_POLICY_STACK $REGION
+
+CLOUDFRONT_CACHE_POLICY_ID=$(aws cloudformation describe-stacks \
+    --stack-name $CLOUDFRONT_CACHE_POLICY_STACK \
+    --region $REGION \
+    --query "Stacks[0].Outputs[?OutputKey=='CachePolicyId'].OutputValue" \
+    --output text)
