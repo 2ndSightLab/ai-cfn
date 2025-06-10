1c459dd200c9515407b9003908f2ba0b487da60a
Teri Radichel <github@pwn.fyi>
2025-06-09 23:47:39 -0400

    Update deploy-cloudfront-dns-records.sh

--- staticweb/scripts/deploy-cloudfront-dns-records.sh
+++ staticweb/scripts/deploy-cloudfront-dns-records.sh
@@ -1,9 +1,12 @@
 
 #!/bin/bash
 
-delete_failed_stack_if_exists $CLOUDFRONT_DNS_STACK $REGION
+read -p "Deploy CloudFront DNS records? (y/n): " DEPLOY_CLOUDFRONT_DNS
+if [[ "$DEPLOY_CLOUDFRONT_DNS" == "y" || "$DEPLOY_CLOUDFRONT_DNS" == "Y" ]]; then
+
+  delete_failed_stack_if_exists $CLOUDFRONT_DNS_STACK $REGION
   
-aws cloudformation deploy \
+  aws cloudformation deploy \
     --template-file cfn/cloudfront-dns-records.yaml \
     --stack-name $CLOUDFRONT_DNS_STACK \
     --parameter-overrides \
@@ -13,4 +16,6 @@ aws cloudformation deploy \
       IncludeWWW=$INCLUDE_WWW \
     --no-fail-on-empty-changeset
     
-stack_exists $CLOUDFRONT_DNS_STACK $REGION $REGION
+  stack_exists $CLOUDFRONT_DNS_STACK $REGION $REGION
+
+fi
