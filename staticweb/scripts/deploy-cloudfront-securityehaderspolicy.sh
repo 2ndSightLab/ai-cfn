384caee9e8e7a07b6fe13fa650dce3fafcde8913
Teri Radichel <github@pwn.fyi>
2025-06-10 16:31:04 -0400

    Update deploy-cloudfront-securityehaderspolicy.sh

--- staticweb/scripts/deploy-cloudfront-securityehaderspolicy.sh
+++ staticweb/scripts/deploy-cloudfront-securityehaderspolicy.sh
@@ -14,7 +14,7 @@ fi
 
 stack_exists $CLOUDFRONT_SECURITYHEADERS_POLICY_STACK $REGION
 
-CLOUDFRONT_CACHE_POLICY_ID=$(aws cloudformation describe-stacks \
+CLOUDFRONT_SECURITYHEADERS_POLICY_ID=$(aws cloudformation describe-stacks \
     --stack-name $CLOUDFRONT_SECURITYHEADERS_POLICY_STACK \
     --region $REGION \
     --query "Stacks[0].Outputs[?OutputKey=='SecurityHeadersPolicyId'].OutputValue" \
