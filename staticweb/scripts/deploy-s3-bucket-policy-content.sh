31e2a9775519557dd112421e626fb2873552ee9e
Teri Radichel <github@pwn.fyi>
2025-06-10 00:37:50 -0400

    Update deploy-s3-bucket-policy-content.sh

--- staticweb/scripts/deploy-s3-bucket-policy-content.sh
+++ staticweb/scripts/deploy-s3-bucket-policy-content.sh
@@ -12,7 +12,7 @@ if [[ "$DEPLOY_POLICY" == "y" || "$DEPLOY_POLICY" == "Y" ]]; then
     --stack-name $S3_POLICY_WEBSITE_STACK \
     --parameter-overrides \
       BucketName=$S3_BUCKET_NAME \
-      OriginAccessIdentityId=$S3_CANONICAL_USER_ID \
+      OriginAccessIdentityId=$OAI_ID \
       CloudFrontDistributionID=$CLOUDFRONT_DISTRIBUTION_ID \
       AccessType=$ACCESS_TYPE \
     --no-fail-on-empty-changeset
