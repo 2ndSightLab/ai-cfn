87e9c1df8d1da07a7a14c33bfe71c0a00aef78b4
Teri Radichel <github@pwn.fyi>
2025-06-10 16:29:05 -0400

    Create deploy-cloudfront-originrequestpolicy.sh

---
+++ staticweb/scripts/deploy-cloudfront-originrequestpolicy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Deploy CloudFront security headers policy? (y/n): " DEPLOY_POLICY
+if [[ "$DEPLOY_POLICY" == "y" || "$DEPLOY_POLICY" == "Y" ]]; then
+
+  delete_failed_stack_if_exists $CLOUDFRONT_ORIGINREQUEST_POLICY_STACK $REGION
+
+  aws cloudformation deploy \
+    --template-file cfn/cloudfront-originrequestpolicy.yaml \
+    --stack-name $CLOUDFRONT_ORIGINREQUEST_POLICY_STACK \
+    --no-fail-on-empty-changeset
+    
+fi
+
+stack_exists $CLOUDFRONT_ORIGINREQUEST_POLICY_STACK $REGION
+
+CLOUDFRONT_SECURITYHEADERS_POLICY_ID=$(aws cloudformation describe-stacks \
+    --stack-name $CLOUDFRONT_ORIGINREQUEST_POLICY_STACK \
+    --region $REGION \
+    --query "Stacks[0].Outputs[?OutputKey=='OriginRequestPolicyId'].OutputValue" \
+    --output text)
