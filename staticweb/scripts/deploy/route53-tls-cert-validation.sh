60eb26d130438c8821fc5d60ab838fe451d4f241
Teri Radichel <github@pwn.fyi>
2025-06-11 15:54:08 -0400

    Update route53-tls-cert-validation.sh

--- staticweb/scripts/deploy/route53-tls-cert-validation.sh
+++ staticweb/scripts/deploy/route53-tls-cert-validation.sh
@@ -12,6 +12,7 @@ HOSTED_ZONE_ID="$3"
 DOMAIN_NAME="$4"
 DOMAIN_TYPE="$5"
 REGION="$6"
+ACM_CERTIFICATE_ARN="$7"
 
 echo "scripts/deploy/route53-validation-dns-records.sh"
 echo "CERT_VALIDATION_STACK_PREFIX=$CERT_VALIDATION_STACK_PREFIX"
@@ -128,12 +129,6 @@ if [ "$SUBDOMAIN" != "" ]; then
 
   CURRENT_VALIDATION_STACK="${CERT_VALIDATION_STACK_PREFIX}-${DOMAIN_TYPE}"
   
-  # Use the cert ARN set in the tls deployment script. If it's not set error out
-  if [ -z "$$ACM_CERTIFICATE_ARN" ]; then
-      echo "No certificate found for $DOMAIN_NAME"
-      exit 1
-  fi
-
   echo "Searching cert for validation record: $ACM_CERTIFICATE_ARN"
 
   VALIDATION_RECORD=$(aws acm describe-certificate --region $REGION --certificate-arn "$ACM_CERTIFICATE_ARN" | \
