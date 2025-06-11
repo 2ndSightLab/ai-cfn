9832cb16a233cadbe2eb777722b8740ec2961b9d
Teri Radichel <github@pwn.fyi>
2025-06-11 12:57:47 -0400

    Update route53-web-dns-records.sh

--- staticweb/scripts/deploy/route53-web-dns-records.sh
+++ staticweb/scripts/deploy/route53-web-dns-records.sh
@@ -44,7 +44,7 @@ if [[ "$DEPLOY_WEB_DNS_RECORDS" == "y" || "$DEPLOY_WEB_DNS_RECORDS" == "Y" ]]; t
   echo "Deploying Route 53 hosted zone for $DOMAIN_NAME..."
   aws cloudformation deploy \
     --region $REGION \
-    --template-file cfn/hosted-zone.yaml \
+    --template-file cfn/route53-web-dns-records.yaml \
     --stack-name $STATIC_WEB_DNS_RECORDS_STACK \
     --parameter-overrides \
       DomainName=$DOMAIN_NAME \
