a2495d6e027577995f0a876766d9c19d3b76fcc7
Teri Radichel <github@pwn.fyi>
2025-06-10 21:46:47 -0400

    Update deploy-cloudfront-originrequestpolicy.sh

--- staticweb/scripts/deploy-cloudfront-originrequestpolicy.sh
+++ staticweb/scripts/deploy-cloudfront-originrequestpolicy.sh
@@ -14,7 +14,7 @@ fi
 
 stack_exists $CLOUDFRONT_ORIGINREQUEST_POLICY_STACK $REGION
 
-CLOUDFRONT_SECURITYHEADERS_POLICY_ID=$(aws cloudformation describe-stacks \
+CLOUDFRONT_ORIGIN_POLICY_ID=$(aws cloudformation describe-stacks \
     --stack-name $CLOUDFRONT_ORIGINREQUEST_POLICY_STACK \
     --region $REGION \
     --query "Stacks[0].Outputs[?OutputKey=='OriginRequestPolicyId'].OutputValue" \
