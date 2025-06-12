a0f31c469236a32c18fc248a48cebf152726f9c9
Teri Radichel <github@pwn.fyi>
2025-06-12 00:23:22 -0400

    Update route53-cloudfront-dns-records.sh

--- staticweb/scripts/deploy/route53-cloudfront-dns-records.sh
+++ staticweb/scripts/deploy/route53-cloudfront-dns-records.sh
@@ -3,21 +3,61 @@
 
 echo "scripts/deploy/route53-cloudfront-dns-records.sh"
 
+function deploy_cloudfront_dns_record(){
+
+  local stack="$1"
+  local domain_name="$2"
+  local hosted_zone_id="$3"
+  local cloudfront_domain="$4"
+  
+  delete_failed_stack_if_exists $stack
+  
+  aws cloudformation deploy \
+    --template-file cfn/route53-cloudfront-dns-record.yaml \
+    --stack-name $stack \
+    --parameter-overrides \
+      DomainName=$domain_name \
+      HostedZoneId=$hosted_zone_id \
+      CloudFrontDomainName=$cloudfront_domain 
+    --no-fail-on-empty-changeset  
+
+   stack_exists $stack $REGION
+ 
+}
+
 read -p "Deploy CloudFront DNS records? (y/n): " DEPLOY_CLOUDFRONT_DNS
 if [[ "$DEPLOY_CLOUDFRONT_DNS" == "y" || "$DEPLOY_CLOUDFRONT_DNS" == "Y" ]]; then
 
-  delete_failed_stack_if_exists $CLOUDFRONT_DNS_STACK $REGION
-  
-  aws cloudformation deploy \
-    --template-file cfn/route53-cloudfront-dns-records.yaml \
-    --stack-name $CLOUDFRONT_DNS_STACK \
-    --parameter-overrides \
-      DomainName=$DOMAIN_NAME \
-      HostedZoneId=$HOSTED_ZONE_ID \
-      CloudFrontDomainName=$CLOUDFRONT_DOMAIN \
-      DomainType=$DOMAIN_TYPE \
-    --no-fail-on-empty-changeset
-    
-  stack_exists $CLOUDFRONT_DNS_STACK $REGION $REGION
+deploy_cloudfront_dns_record \
+  $CLOUDFRONT_DNS_STACK \
+  $DOMAIN_NAME \
+  $HOSTED_ZONE_ID \ 
+  $CLOUDFRONT_DOMAIN 
 
+  if [ "$DOMAIN_TYPE" == "WWW" ]; then 
+  
+    deploy_cloudfront_dns_record \
+      ${CLOUDFRONT_DNS_STACK}-WWW \
+      $DOMAIN_NAME \
+      $HOSTED_ZONE_ID \ 
+      $CLOUDFRONT_DOMAIN 
+  
+  fi
+
+  if [ "$DOMAIN_TYPE" == "Wildcard" ]; then 
+
+    deploy_cloudfront_dns_record \
+      ${CLOUDFRONT_DNS_STACK}-Wildcard \
+      $DOMAIN_NAME \
+      $HOSTED_ZONE_ID \ 
+      $CLOUDFRONT_DOMAIN 
+  fi  
+
+  if [ "$DOMAIN_TYPE" == "Subdomains" ]; then 
+  
+    echo "Not implemented: Loop through subdomains and deploy each one"
+    exit
+    
+  fi  
+  
 fi
