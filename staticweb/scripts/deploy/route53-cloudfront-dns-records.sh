374de0c7426b274ecb435e6f44370e6950c87809
Teri Radichel <github@pwn.fyi>
2025-06-12 10:56:09 -0400

    Update route53-cloudfront-dns-records.sh

--- staticweb/scripts/deploy/route53-cloudfront-dns-records.sh
+++ staticweb/scripts/deploy/route53-cloudfront-dns-records.sh
@@ -28,9 +28,9 @@ function deploy_cloudfront_dns_record(){
     --parameter-overrides \
       DomainName=$domain_name \
       HostedZoneId=$hosted_zone_id \
-      CloudFrontDomainName=$cloudfront_domain 
-    --no-fail-on-empty-changeset  
-    --region $region
+      CloudFrontDomainName=$cloudfront_domain \
+    --no-fail-on-empty-changeset \
+    --region $region 
 
    stack_exists $stack $region
  
