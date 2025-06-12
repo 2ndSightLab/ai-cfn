9889e1c0416fd4bd28195a2bd2816d47bf7c299b
Teri Radichel <github@pwn.fyi>
2025-06-12 11:18:26 -0400

    Update route53-cloudfront-dns-records.sh

--- staticweb/scripts/deploy/route53-cloudfront-dns-records.sh
+++ staticweb/scripts/deploy/route53-cloudfront-dns-records.sh
@@ -14,12 +14,6 @@ function deploy_cloudfront_dns_record(){
   cloudfront_domain="$4"
   region="$5"
 
-  echo "stack: $1"
-  echo "domain_name: $2"
-  echo "hosted_zone_id: $3"
-  echo "cloudfront_domain: $4"
-  echo "region: $5"
-  
   delete_failed_stack_if_exists $stack $region
   
   aws cloudformation deploy \
@@ -39,32 +33,48 @@ function deploy_cloudfront_dns_record(){
 read -p "Deploy CloudFront DNS records? (y/n): " DEPLOY_CLOUDFRONT_DNS
 if [[ "$DEPLOY_CLOUDFRONT_DNS" == "y" || "$DEPLOY_CLOUDFRONT_DNS" == "Y" ]]; then
 
+  if [ "$DOMAIN_NAME" == "" ]; then
+     echo "DOMAIN_NAME is not set: $DOMAIN_NAME"
+  fi
+  
+  if [ "$HOSTED_ZONE_ID" == "" ]; then
+     echo "HOSTED_ZONE_ID is not set: $HOSTED_ZONE_ID"
+  fi
+  
+  if [ "$CLOUDFRONT_DOMAIN" == "" ]; then
+     echo "CLOUDFRONT_DOMAIN is not set: $CLOUDFRONT_DOMAIN"
+  fi
+
+  if [ "$REGION" == "" ]; then
+     echo "REGION is not set: $REGION"
+  fi
+  
   deploy_cloudfront_dns_record \
-    $CLOUDFRONT_DNS_STACK \
-    $DOMAIN_NAME \
+    "$CLOUDFRONT_DNS_STACK" \
+    "$DOMAIN_NAME" \
     "$HOSTED_ZONE_ID" \
-    $CLOUDFRONT_DOMAIN \
-    $REGION
+    "$CLOUDFRONT_DOMAIN" \
+    "$REGION"
 
   if [ "$DOMAIN_TYPE" == "WWW" ]; then 
   
     deploy_cloudfront_dns_record \
-      ${CLOUDFRONT_DNS_STACK}-WWW \
-      $DOMAIN_NAME \
+      "$CLOUDFRONT_DNS_STACK" \
+      "$DOMAIN_NAME" \
       "$HOSTED_ZONE_ID" \
-      $CLOUDFRONT_DOMAIN \
-      $REGION
+      "$CLOUDFRONT_DOMAIN" \
+      "$REGION"
   
   fi
 
   if [ "$DOMAIN_TYPE" == "Wildcard" ]; then 
 
     deploy_cloudfront_dns_record \
-      ${CLOUDFRONT_DNS_STACK}-Wildcard \
-      $DOMAIN_NAME \
+      "$CLOUDFRONT_DNS_STACK" \
+      "$DOMAIN_NAME" \
       "$HOSTED_ZONE_ID" \
-      $CLOUDFRONT_DOMAIN \
-      $REGION
+      "$CLOUDFRONT_DOMAIN" \
+      "$REGION"
   fi  
 
   if [ "$DOMAIN_TYPE" == "Subdomains" ]; then 
