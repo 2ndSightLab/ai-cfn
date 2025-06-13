a07e0c2dc407d097d04b0accf9c0d9178dc33dbb
Teri Radichel <github@pwn.fyi>
2025-06-12 23:23:30 -0400

    Update cloudshell-command.sh

--- staticweb/cloudshell-command.sh
+++ staticweb/cloudshell-command.sh
@@ -1,5 +1,5 @@
-#use these commands to run the scripts in the AWS account 
-#where the website exists
+# run these commands in the AWS account 
+# where you want to deploy the website
 cd ~
 rm -rf ai-cfn
 git clone https://github.com/2ndSightLab/ai-cfn.git
@@ -8,9 +8,10 @@ chmod 700 deploy.sh
 chmod 700 scripts/deploy/route53-tls-cert-validation.sh
 ./deploy.sh
 
-#use these commands to run the script to update the 
-#name servers where the primary domain exists
-#for a non-production domain only 
+# run these commands to update the 
+# name servers where the primary domain exists
+# for a non-production domain only 
+# entering the name servers output by the above script
 cd ~
 rm -rf ai-cfn
 git clone https://github.com/2ndSightLab/ai-cfn.git
