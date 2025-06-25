5e3d5a6327babbe2bed55d1fb451794501f681c7
Teri Radichel <github@pwn.fyi>
2025-06-24 20:03:52 -0400

    Update get-latest-ami.sh

--- ec2/scripts/get-latest-ami.sh
+++ ec2/scripts/get-latest-ami.sh
@@ -195,9 +195,9 @@ echo "AMI_ID=$(aws ec2 describe-images \
     --region $REGION \
     --owners $OWNER \
     --filters \
-    "Name=name,Values=$OS_FILTER" \
-    "Name=state,Values=available" \
-    "Name=architecture,Values=$ARCHITECTURE" \
+    'Name=name,Values=$OS_FILTER' \
+    'Name=state,Values=available' \
+    'Name=architecture,Values=$ARCHITECTURE' \
     --query 'sort_by(Images, &CreationDate)[-1].ImageId' \
     --output text)"
     
