e9565604c66e94219eb7f9bcd9c845cf42025aee
Teri Radichel <github@pwn.fyi>
2025-06-25 17:36:44 -0400

    Update run.sh

--- ec2/run.sh
+++ ec2/run.sh
@@ -7,9 +7,6 @@ AMI_ID=""
 #get the latest ami id for the selected OS
 source scripts/get-latest-ami.sh
 
-if [ "$REGION" == "" ]; then echo "Error: Region is not set in get-latest-ami.sh"; exit; fi
-if [ "$AMI_ID" == "" ]; then echo "Error: AMI ID is not set in get-latest-ami.sh"; exit; fi
-
 #get the instance type to use when launching the instance
 source scripts/get-ec2-instance-type.sh
 
