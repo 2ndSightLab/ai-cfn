7a603ca62b8e3cc10c9bf2041ef02345563fb83c
Teri Radichel <github@pwn.fyi>
2025-06-25 18:39:25 -0400

    Update run.sh

--- ec2/run.sh
+++ ec2/run.sh
@@ -3,6 +3,7 @@
 #temp for testing
 REGION=""
 AMI_ID=""
+INSTANCE_TYPE=""
 
 #get the latest ami id for the selected OS
 source scripts/get-latest-ami.sh
@@ -10,3 +11,6 @@ source scripts/get-latest-ami.sh
 #get the instance type to use when launching the instance
 source scripts/get-ec2-instance-type.sh
 
+echo "REGION: $REGION"
+echo "AMI_ID: $AMI_ID"
+echo "INSTANCE_TYPE: $INSTANCE_TYPE"
