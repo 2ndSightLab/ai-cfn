c8943b2ad2ae4e2d380694af0accdf8906ef7df6
Teri Radichel <github@pwn.fyi>
2025-06-25 17:10:00 -0400

    Update run.sh

--- ec2/run.sh
+++ ec2/run.sh
@@ -4,5 +4,9 @@
 REGION=us-east-2
 AMI_ID=""
 
+#get the latest ami id for the selected OS
+scripts/get-latest-ami.sh
+
+#get the instance type to use when launching the instance
 source scripts/get-ec2-instance-type.sh
 
