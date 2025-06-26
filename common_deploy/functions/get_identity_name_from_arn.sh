baff4b230f9489f322e5c7e2c70ddafa98251b7b
Teri Radichel <github@pwn.fyi>
2025-06-26 13:41:17 -0400

    Update get_identity_name_from_arn.sh

--- common_deploy/functions/get_identity_name_from_arn.sh
+++ common_deploy/functions/get_identity_name_from_arn.sh
@@ -14,35 +14,46 @@ get_identity_name_from_arn() {
     # Get the resource type and name
     local resource_path=${arn_parts[5]}
     
+    # Debug output to see what's happening
+    # echo "DEBUG: ARN=$arn" >&2
+    # echo "DEBUG: resource_path=$resource_path" >&2
+    
     # Handle different ARN formats
     case $resource_path in
         "root")
             echo "root"
             ;;
-        "user"*)
+        user*)
             # For IAM users: arn:aws:iam::123456789012:user/username
-            local username=${arn#*:user/}
+            # Remove quotes around pattern to match more broadly
+            local username=${resource_path#user/}
             echo "$username"
             ;;
-        "role"*)
+        role*)
             # For IAM roles: arn:aws:iam::123456789012:role/rolename
-            local rolename=${arn#*:role/}
+            local rolename=${resource_path#role/}
             echo "$rolename"
             ;;
         "assumed-role"*)
             # For assumed roles: arn:aws:sts::123456789012:assumed-role/rolename/sessionname
-            local role_session=${arn#*:assumed-role/}
+            local role_session=${resource_path#assumed-role/}
             local rolename=${role_session%%/*}
             echo "$rolename"
             ;;
         "federated-user"*)
             # For federated users: arn:aws:sts::123456789012:federated-user/username
-            local username=${arn#*:federated-user/}
+            local username=${resource_path#federated-user/}
             echo "$username"
             ;;
         *)
-            echo "Error: Unsupported ARN type." >&2
-            return 1
+            # If all else fails, try to extract a username from the last part of the ARN
+            if [[ "$arn" == *:user/* ]]; then
+                local username=${arn##*:user/}
+                echo "$username"
+            else
+                echo "Error: Unsupported ARN type: $resource_path" >&2
+                return 1
+            fi
             ;;
     esac
 }
