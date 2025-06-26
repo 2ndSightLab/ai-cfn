fca266866f83ace77797f5e5cdebe0a5f53d80f6
Teri Radichel <github@pwn.fyi>
2025-06-26 13:03:30 -0400

    Create is_valid_aws_service.sh

---
+++ common_deploy/functions/is_valid_aws_service.sh
@@ -0,0 +1,26 @@
+is_valid_aws_service() {
+    local service_name=$1
+    
+    # Check if service name is provided
+    if [ -z "$service_name" ]; then
+        echo "Error: Service name must be provided." >&2
+        return 1
+    fi
+    
+    # Fetch the list of valid AWS service names
+    local aws_services=$(curl -s https://servicereference.us-east-1.amazonaws.com/v1/service-list.json | jq -r '.services[].id')
+    
+    # Check if curl or jq command failed
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to retrieve AWS service list." >&2
+        return 1
+    fi
+    
+    # Check if the service name is in the list
+    if [[ $aws_services =~ (^|[[:space:]])$service_name($|[[:space:]]) ]]; then
+        return 0  # Valid service
+    else
+        echo "Error: '$service_name' is not a valid AWS service." >&2
+        return 1  # Invalid service
+    fi
+}
