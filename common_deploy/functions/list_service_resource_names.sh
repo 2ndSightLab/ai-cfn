bd9062eeb3ea9870f32ad23cfbde099110095515
Teri Radichel <github@pwn.fyi>
2025-06-26 14:40:08 -0400

    Update list_service_resource_names.sh

--- common_deploy/functions/list_service_resource_names.sh
+++ common_deploy/functions/list_service_resource_names.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-
-# Function to check if a service is a valid AWS service
-is_valid_aws_service() {
-  SERVICE_NAME="$1"
-  SERVICE_LIST_URL="https://servicereference.us-east-1.amazonaws.com/v1/service-list.json"
-  
-  # Check if the service exists in the service list
-  SERVICE_CHECK=$(curl -s "$SERVICE_LIST_URL" | jq -r '.[] | select(.service == "'$SERVICE_NAME'") | .service')
-  
-  if [ "$SERVICE_CHECK" = "$SERVICE_NAME" ]; then
-    return 0  # Service is valid (success)
-  else
-    return 1  # Service is not valid (failure)
-  fi
-}
-
 list_service_resource_names() {
   SERVICE_NAME="$1"
   
