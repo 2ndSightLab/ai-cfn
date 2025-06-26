24e10da99010335cfbfe25798365f458ab90be3a
Teri Radichel <github@pwn.fyi>
2025-06-26 15:06:22 -0400

    Update list_service_resource_names.sh

--- common_deploy/functions/list_service_resource_names.sh
+++ common_deploy/functions/list_service_resource_names.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
 list_service_resource_names() {
-  SERVICE_NAME="$1"
+  SERVICE_NAME=$(echo "$1" | tr '[:lower:]' '[:upper:]')
   
-  aws cloudformation list-types --visibility PUBLIC --type RESOURCE | jq -r '.TypeSummaries[] | select(.TypeName | startswith("AWS::$SERVICE_NAME::")) | .TypeName | sub("AWS::$SERVICE_NAME::";"")' 
-
+  aws cloudformation list-types --visibility PUBLIC --type RESOURCE | jq -r --arg svc "$SERVICE_NAME" '.TypeSummaries[] | select(.TypeName | startswith("AWS::" + $svc + "::")) | .TypeName | sub("AWS::" + $svc + "::";"")' 
 }
