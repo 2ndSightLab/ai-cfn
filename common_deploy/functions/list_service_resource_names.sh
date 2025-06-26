35e8ae43a7cf4283d26312913d8987f3a14e1699
Teri Radichel <github@pwn.fyi>
2025-06-26 19:45:31 -0400

    Update list_service_resource_names.sh

--- common_deploy/functions/list_service_resource_names.sh
+++ common_deploy/functions/list_service_resource_names.sh
@@ -1,5 +1,5 @@
 list_service_resource_names() {
-  SERVICE_NAME=$(echo "$1"
+  SERVICE_NAME="$1"
 
   aws cloudformation list-types --visibility PUBLIC --type RESOURCE --filters Type=RESOURCE_TYPE_NAME,Value=AWS::${CF_SERVICE_NAME}::*
 
