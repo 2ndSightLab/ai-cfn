c4da04b514c19539790b6ba036570ec8f90e6c4d
Teri Radichel <github@pwn.fyi>
2025-06-25 22:53:08 -0400

    Update run.sh

--- network/run.sh
+++ network/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "Create network - in progress"
+echo "Enter the name of the environment for which you want to deploy a shared network:"
+read ENV_NAME
+
+user=$(aws sts get-caller-identity --query User.Arn --output text | cut -d '/' -f 2)
+
+echo "Enter VPC_CIDR (e.g. 10.20.30.0/23"
+read VPC_CIDR
+
+VPC_NAME="$env-VPC"
+ENABLE_DNS_SUPPORT="true"
+ENABLE_DNS_HOSTNAMES="false"
+TEMPLATE_FILE="cfn/vpc.yaml"
+STACK_NAME=$VPC_NAME
+
+
+IGW_NAME="$ENV_NAME-IGW"
