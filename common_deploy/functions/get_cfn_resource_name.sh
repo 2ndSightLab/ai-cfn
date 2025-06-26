0fca23922884cef6e177ce6fbc9b508227a25bd0
Teri Radichel <github@pwn.fyi>
2025-06-26 13:02:59 -0400

    Create get_cfn_resource_name.sh

---
+++ common_deploy/functions/get_cfn_resource_name.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+get_cfn_resource_name() {
+    local ENV_NAME=$1
+    local SERVICE=$2
+    local RESOURCE=$3
+    local NAME=$4
+
+    # Check if all parameters are provided
+    if [ -z "$ENV_NAME" ] || [ -z "$SERVICE" ] || [ -z "$RESOURCE" ] ; then
+        echo "Error: All parameters (ENV_NAME, SERVICE, RESOURCE) must be provided." >&2
+        return 1
+    fi
+
+    # Return the concatenated string
+    resource_name="$ENV_NAME-$SERVICE-$RESOURCE"
+
+    if [ "$NAME != "" ]; then
+        resource_name="$resource_name-$NAME"
+    fi
+}
