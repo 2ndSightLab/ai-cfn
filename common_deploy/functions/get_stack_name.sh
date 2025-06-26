8203e227d20c26cdd281cfcbc66beae868e71ebd
Teri Radichel <github@pwn.fyi>
2025-06-26 15:22:56 -0400

    Update get_stack_name.sh

--- common_deploy/functions/get_stack_name.sh
+++ common_deploy/functions/get_stack_name.sh
@@ -4,7 +4,7 @@ get_stack_name() {
     local IDENTITY_NAME=$(echo "$2" | tr '[:upper:]' '[:lower:]')
     local SERVICE=$(echo "$3" | tr '[:upper:]' '[:lower:]')
     local RESOURCE==$(echo "$4" | tr '[:upper:]' '[:lower:]')
-    local NAME==$(echo "$5" | tr '[:upper:]' '[:lower:]')
+    local NAME=$(echo "$5" | tr '[:upper:]' '[:lower:]')
 
     # Check if all parameters are provided
     if [ -z "$ENV_NAME" ] || [ -z "$IDENTITY_NAME" ] || [ -z "$SERVICE" ] || [ -z "$RESOURCE" ] ; then
