2f0880892e87be9b4e016f4ca0a17c2185cef8fe
Teri Radichel <github@pwn.fyi>
2025-06-26 19:58:33 -0400

    Update is_valid_aws_service.sh

--- common_deploy/functions/is_valid_aws_service.sh
+++ common_deploy/functions/is_valid_aws_service.sh
@@ -2,28 +2,18 @@
 
 is_valid_aws_service() {
     local service_name="$1"
+    
     # Check if service name is provided
     if [ -z "$service_name" ]; then
         echo "Error: Service name must be provided." >&2
         exit
     fi
     
-    # Fetch the list of valid AWS service names
-    local aws_services=$(curl -s https://servicereference.us-east-1.amazonaws.com/v1/service-list.json | jq -r '.[].service')
-    
-    # Check if curl or jq command failed
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to retrieve AWS service list." >&2
-        exit
-    fi
-    
-    # Check if the service name is in the list
-    if [[ $aws_services =~ (^|[[:space:]])$service_name($|[[:space:]]) ]]; then
-        return 0  # Valid service
+    if aws cloudformation list-types --visibility PUBLIC --type RESOURCE --filters TypeNamePrefix=AWS::${SERVICE_NAME}:: --query 'length(TypeSummaries)' --output text | grep -q -v ^0$; then
+        echo "${SERVICE_NAME} service exists"
     else
-        echo "Error: '$service_name' is not a valid AWS service. Enter for a list of service names" >&2
-        read ok
-        list_service_names
+        echo "${SERVICE_NAME} service does not exist"
+        echo "Service names are CASE SENSITIVE and need to match proper CloudFormation upper and lower case."
         exit
     fi
 }
