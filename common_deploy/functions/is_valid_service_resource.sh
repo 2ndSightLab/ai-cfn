cba69aa55a11c8239c23ff86140291eb8c32d93e
Teri Radichel <github@pwn.fyi>
2025-06-26 15:34:47 -0400

    Update is_valid_service_resource.sh

--- common_deploy/functions/is_valid_service_resource.sh
+++ common_deploy/functions/is_valid_service_resource.sh
@@ -6,7 +6,7 @@ is_valid_service_resource() {
   RESOURCE_EXISTS=$(aws cloudformation list-types --visibility PUBLIC --type RESOURCE | \
   jq -r --arg svc "$SERVICE_NAME" --arg res "$RESOURCE_NAME" '
     .TypeSummaries[] 
-    | select(.TypeName == "AWS::" + $svc + "::" + $res) 
+    | select((.TypeName | ascii_upcase) == ("AWS::" + $svc + "::" + $res)) 
     | .TypeName
   ')
 
@@ -18,4 +18,3 @@ is_valid_service_resource() {
     return 0
   fi
 }
-
