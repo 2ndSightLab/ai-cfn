e3b5fed7152f613d11137ab5a36338ae34ad05c8
Teri Radichel <github@pwn.fyi>
2025-06-26 19:40:47 -0400

    Update list_service_names.sh

--- common_deploy/functions/list_service_names.sh
+++ common_deploy/functions/list_service_names.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 list_service_names(){
-  # Fetch the service list and process it with grep and sed
-  echo "Available AWS services:"
-  curl https://servicereference.us-east-1.amazonaws.com/v1/service-list.json | grep -v servicereference | grep service | sed 's|"service" : "||g' | sed 's|"||g' | sed 's|,||'
+  aws cloudformation list-types --visibility PUBLIC --type RESOURCE --output json | jq -r '.TypeSummaries[].TypeName' | grep -o 'AWS::[^:]*' | sed 's/AWS:://' | sort -u
 }
