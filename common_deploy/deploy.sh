6d3d1b3a8b19d4f63a049826c479df28f6c52838
Teri Radichel <github@pwn.fyi>
2025-06-26 14:16:42 -0400

    Update deploy.sh

--- common_deploy/deploy.sh
+++ common_deploy/deploy.sh
@@ -26,7 +26,7 @@ while [ -z "$RESOURCE_NAME" ]; do
     echo "Enter the resource of the service $SERVICE_NAME that you want to deploy (type help for a list of resources):"
     read RESOURCE_NAME
     if [ "$RESOURCE_NAME" == "help" ]; then
-       list_resources_for_service $SERVICE_NAME
+       list_service_resource_names $SERVICE_NAME
        RESOURCE_NAME=""
     fi
 done
