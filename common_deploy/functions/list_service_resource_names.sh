8bf3214b82ecac2f5de2ff7b5127080d897acc32
Teri Radichel <github@pwn.fyi>
2025-06-26 15:03:39 -0400

    Update list_service_resource_names.sh

--- common_deploy/functions/list_service_resource_names.sh
+++ common_deploy/functions/list_service_resource_names.sh
@@ -2,31 +2,6 @@
 list_service_resource_names() {
   SERVICE_NAME="$1"
   
-  # Check if the service is valid
-  if ! is_valid_aws_service "$SERVICE_NAME"; then
-    echo "Service '$SERVICE_NAME' not found in the service list."
-    echo "Available services:"
-    curl -s "https://servicereference.us-east-1.amazonaws.com/v1/service-list.json" | jq -r '.[] | .service' | sort
-    exit 1  # Exit with error code
-  fi
-  
-  # Service exists, now get its resources
-  SERVICE_URL="https://servicereference.us-east-1.amazonaws.com/v1/${SERVICE_NAME}/${SERVICE_NAME}.json"
+  aws cloudformation list-types --visibility PUBLIC --type RESOURCE | jq -r '.TypeSummaries[] | select(.TypeName | startswith("AWS::$SERVICE_NAME::")) | .TypeName | sub("AWS::$SERVICE_NAME::";"")' 
 
-  echo "Retriving data from $SERVICE_URL"
-  
-  echo "Resources for $SERVICE_NAME:"
-  
-  # Get the service JSON
-  SERVICE_JSON=$(curl -s "$SERVICE_URL")
-  
-  # Check if ResourceTypes exists and has keys
-  RESOURCE_COUNT=$(echo "$SERVICE_JSON" | jq '.ResourceTypes | keys | length')
-  
-  if [ "$RESOURCE_COUNT" -gt 0 ]; then
-    # List all resource types
-    echo "$SERVICE_JSON" | jq -r '.ResourceTypes | keys[]'
-  else
-    echo "This service has no resources"
-  fi
 }
