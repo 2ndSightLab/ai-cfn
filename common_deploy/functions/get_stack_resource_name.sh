dbed8ed08b2d905974ba645990e312f2b08b16aa
Teri Radichel <github@pwn.fyi>
2025-06-26 20:52:12 -0400

    Update get_stack_resource_name.sh

--- common_deploy/functions/get_stack_resource_name.sh
+++ common_deploy/functions/get_stack_resource_name.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 get_stack_resource_name() {
-    local ENV_NAME=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-    local SERVICE=$(echo "$2" | tr '[:upper:]' '[:lower:]')
-    local RESOURCE=$(echo "$3" | tr '[:upper:]' '[:lower:]')
-    local NAME=$(echo "$4" | tr '[:upper:]' '[:lower:]')
+    local ENV_NAME="$1" 
+    local SERVICE="$2" 
+    local RESOURCE="$3"
+    local NAME="$4"
 
     # Check if all parameters are provided
     if [ -z "$ENV_NAME" ] || [ -z "$SERVICE" ] || [ -z "$RESOURCE" ] ; then
@@ -16,6 +16,12 @@ get_stack_resource_name() {
     
     #validte the resource is a valid AWS service resource
     is_valid_service_resource "$SERVICE" "$RESOURCE"
+
+    ENV_NAME=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+    IDENTITY_NAME=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+    SERVICE=$(echo "$3" | tr '[:upper:]' '[:lower:]')
+    RESOURCE=$(echo "$4" | tr '[:upper:]' '[:lower:]')
+    NAME=$(echo "$5" | tr '[:upper:]' '[:lower:]')
 
     # Return the concatenated string
     resource_name="$ENV_NAME-$SERVICE-$RESOURCE"
