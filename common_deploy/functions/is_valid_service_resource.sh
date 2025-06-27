c2ccc787e717f8732ac128c529d5a338446beab2
Teri Radichel <github@pwn.fyi>
2025-06-26 20:06:31 -0400

    Update is_valid_service_resource.sh

--- common_deploy/functions/is_valid_service_resource.sh
+++ common_deploy/functions/is_valid_service_resource.sh
@@ -4,13 +4,11 @@ is_valid_service_resource() {
   SERVICE_NAME="$1"
   RESOURCE_NAME="$2"
 
-  RESOURCE_EXISTS=$(aws cloudformation list-types --visibility PUBLIC --type RESOURCE | \
-  jq -r --arg svc "$SERVICE_NAME" --arg res "$RESOURCE_NAME" '
-    .TypeSummaries[] 
-    | select((.TypeName | ascii_upcase) == ("AWS::" + $svc + "::" + $res)) 
-    | .TypeName
-  ')
-
+  RESOURCE_EXISTS=$(aws cloudformation list-types --visibility PUBLIC --type RESOURCE \
+  --filters TypeNamePrefix="AWS::${SERVICE_NAME}::${RESOURCE_NAME}" \
+  --query "TypeSummaries[?TypeName=='AWS::${SERVICE_NAME}::${RESOURCE_NAME}'].TypeName" \
+  --output text)
+  
   if [ -z "$RESOURCE_EXISTS" ]; then
     echo "Error: Invalid resource name '$RESOURCE_NAME' for service '$SERVICE_NAME'"
     exit
