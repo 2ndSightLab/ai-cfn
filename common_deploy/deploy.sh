72063e0787e68e0349a76a2ec1abd62ea31fc550
Teri Radichel <github@pwn.fyi>
2025-06-26 22:05:17 -0400

    Update deploy.sh

--- common_deploy/deploy.sh
+++ common_deploy/deploy.sh
@@ -54,6 +54,7 @@ echo "REGION: $REGION"
 echo "STACK_NAME: $STACK_NAME"
 echo "STACK_RESOURCE_NAME: $STACK_RESOURCE_NAME"
 
+create_cloudformation_template $SERVICE_NAME $RESOURCE_NAME
 create_deploy_code_for_resource $SERVICE_NAME $RESOURCE_NAME
 
 local SCRIPT_FILE_PATH=$(get_script_file_path $SERVICE_NAME $RESOURCE_NAME)
