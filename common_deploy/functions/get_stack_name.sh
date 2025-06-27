99c1ee29d7f0f4bf747cbefc054d8b7a89e92241
Teri Radichel <github@pwn.fyi>
2025-06-26 20:46:15 -0400

    Update get_stack_name.sh

--- common_deploy/functions/get_stack_name.sh
+++ common_deploy/functions/get_stack_name.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -e
 get_stack_name() {
-    local ENV_NAME=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-    local IDENTITY_NAME=$(echo "$2" | tr '[:upper:]' '[:lower:]')
-    local SERVICE=$(echo "$3" | tr '[:upper:]' '[:lower:]')
-    local RESOURCE=$(echo "$4" | tr '[:upper:]' '[:lower:]')
-    local NAME=$(echo "$5" | tr '[:upper:]' '[:lower:]')
+    local ENV_NAME="$1"
+    local IDENTITY_NAME="$2"
+    local SERVICE="$3"
+    local RESOURCE="$4"
+    local NAME="$5"
 
     # Check if all parameters are provided
     if [ -z "$ENV_NAME" ] || [ -z "$IDENTITY_NAME" ] || [ -z "$SERVICE" ] || [ -z "$RESOURCE" ] ; then
         echo "Error: All parameters (ENV_NAME, USERNAME, SERVICE, RESOURCE) must be provided." >&2
         exit
     fi
+
+    ENV_NAME=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+    IDENTITY_NAME=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+    SERVICE=$(echo "$3" | tr '[:upper:]' '[:lower:]')
+    RESOURCE=$(echo "$4" | tr '[:upper:]' '[:lower:]')
+    NAME=$(echo "$5" | tr '[:upper:]' '[:lower:]')
     
     # Return the concatenated string
     echo "$ENV_NAME-$IDENTITY_NAME-$SERVICE-$RESOURCE-$NAME"
