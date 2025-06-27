04f965e76f39cd1448c5f7a3f45af242b74e4dec
Teri Radichel <github@pwn.fyi>
2025-06-26 21:16:11 -0400

    Update get_script_file_path.sh

--- common_deploy/functions/get_script_file_path.sh
+++ common_deploy/functions/get_script_file_path.sh
@@ -3,5 +3,13 @@ get_script_file_path(){
     local RESOURCE_NAME="$1" 
     local SERVICE_NAME="$2"
   
+    local DIR_PATH="resources/$SERVICE_NAME"
+    
+    # Create the directory structure if it doesn't exist
+    if [ ! -d "$DIR_PATH" ]; then
+        mkdir -p "$DIR_PATH"
+        echo "Created directory: $DIR_PATH"
+    fi
+  
     echo "resources/$SERVICE_NAME/$RESOURCE_NAME.sh"
 }
